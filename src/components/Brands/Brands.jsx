0a376f90a599d7bc1af058dc97ec4f25f65c5741
Ahmed Emam <ahmed.emam12380@gmail.com>
2024-01-01 00:29:30 +0200

    add condition to brands because data not displayed

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -7,8 +7,7 @@ export default function Brands() {
 
   let { loading, error, brands } = useSelector((state) => state.brands);
   let dispatch = useDispatch()
-  // console.log(loading);
-  // console.log(error);
+
   // console.log(brands);
 
 
@@ -36,14 +35,14 @@ export default function Brands() {
       <div className="container mb-5">
         <div className="row">
           <h3 className='h2 brand-title pt-4'>All Brands</h3>
-          {brands? brands.map((brand) => {
+          {brands = []? <h2>There is proplem in api's</h2> : brands.map((brand) => {
             return <div key={brand._id} className="brand">
 
               <img src={brand.image} alt="" className="w-100" />
               <h4 className="h6 fw-bold">{brand.name}</h4>
 
             </div>
-          }) : "There is proplem in api's"}
+          })}
         </div>
       </div>
     </>
